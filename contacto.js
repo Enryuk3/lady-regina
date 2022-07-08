f47e402140b90e1597135d3bbf76538fffd2ec58
Federico Errecalde <federicoerrecalde@gmail.com>
2022-07-07 21:19:50 -0300

    ADD Form validation

--- contacto.js
+++ contacto.js
@@ -10,6 +10,19 @@ if (email.length < 5 || email.indexOf("@") == -1 || email.indexOf(".") == -1) {e
 if (telefono.length < 7) {errorTelefono.innerHTML = `<p class="bg-danger parrafo text-white p-3 rounded">Ingrese un telefono válido</p>`} else { errorTelefono.innerHTML = ""};
 if (mensaje.length < 10) {errorMensaje.innerHTML = `<p class="bg-danger parrafo text-white p-3 rounded">Ingrese su mensaje</p>`} else { errorMensaje.innerHTML = ""};
  
-(email.length < 5) || (email.indexOf("@") == -1) || (email.indexOf(".") == -1) || (nombre.length < 5) || (telefono.length < 7) || (mensaje.length < 10)? errorForm.innerHTML = `<p class="bg-danger col-12 col-md-12 mx-auto text-white parrafo ps-3 py-2 pe-4  p-lg-3 rounded">Revise los campos requeridos!</p>` : errorForm.innerHTML = `<p class="mx-auto bg-purple text-white  ps-3 py-2 pe-4 parrafo p-lg-3 rounded">Gracias por comunicarte con nosotros!</p>`;
+(email.length < 5) || (email.indexOf("@") == -1) || (email.indexOf(".") == -1) || (nombre.length < 5) || (telefono.length < 7) || (mensaje.length < 10)? errorForm.innerHTML = `<p class="bg-danger col-12 col-md-12 mx-auto text-white parrafo ps-3 py-2 pe-4 mt-3 p-lg-3 rounded">Revise los campos requeridos!</p>` : errorForm.innerHTML = `<p class="mx-auto bg-turquoise text-purple  ps-3 py-2 pe-4 parrafo mt-3 p-lg-3 rounded">Gracias por comunicarte con nosotros!</p>`;
 }
-    document.getElementById("boton_enviar").addEventListener("click", validarFormulario);
+document.getElementById("boton_enviar").addEventListener("click", validarFormulario);
+
+
+boton_borrar.addEventListener('click', () => {
+    nombreCompleto.value = "";
+    email.value= "";
+    telefono.value= "";
+    mensaje.value= "";
+    errorNombre.innerHTML= "";
+    errorEmail.innerHTML = "";
+    errorTelefono.innerHTML = "";
+    errorMensaje.innerHTML = "";
+    errorForm.innerHTML = "";
+  })
